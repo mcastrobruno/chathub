c9ff3fa6f6bd03b7f836d90dc9ef45a8a3932e4c
Bruno Castro <mcastrobruno@gmail.com>
2018-04-03 10:39:48 -0300

    Initial commit

---
+++ ChatHub.Server/App_Code/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+public class ChatHub : Hub
+{
+    public void Send(string name, string message)
+    {
+        string messageId = Guid.NewGuid().ToString();
+
+        Clients.All.broadcastMessage(name, message, messageId);
+    }
+
+    public void Command(string name)
+    {
+        Clients.All.sendCommand(name);
+    }
+
+    public void Acknowledge(string messageId)
+    {
+        Clients.All.broadcastAcknowledge(messageId);
+    }
+}
